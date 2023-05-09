6a1dc61843792f90596590e13ec93f8afcf57327
ye21st <ye21st@gmail.com>
2023-05-09 18:21:57 +0800

    feat: 第一次提交

---
+++ android/app/src/main/kotlin/com/focre/learnlayout/learn_flutter_layout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.focre.learnlayout.learn_flutter_layout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/01/01/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      // 紧约束，使用的屏幕尺寸
+      const ColoredBox(color: Colors.blue)
+  );
+}
